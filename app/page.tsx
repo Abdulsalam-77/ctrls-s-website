0928e4caa4c63106ff9a82ca50be0a1e0b23b703
ABDULSALAM MOHAMED ELSAYED <A.Mohamed2222@outlook.com>
2025-08-03 16:22:11 +0300

    change in hero text

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function HomePage() {
       <WhyCtrlsSSection />
       <RoadmapSection />
       <CertificateShowcase />
+      {/* test */}
       <HomeCtaSection />
     </>
   )
